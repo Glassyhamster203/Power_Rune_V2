391e72caa2a008ee12939098bd6414b18bddf895
zencode <1056246767@qq.com>
2022-09-19 23:01:23 +0800

    modified:   Framework/Power_Rune_V2/Power_Rune_V2.ino

--- Framework/Power_Rune_V2/Power_Rune_V2.ino
+++ Framework/Power_Rune_V2/Power_Rune_V2.ino
@@ -19,23 +19,124 @@
 #define ADC_Val 1000
 #define ADC_PIN 2
 
+#define LED_STRIP_COLUMN 8
+#define LED_STRIP_ROW 40
+
 #define LED_BRI
 
 char *ssid = (char *)"robomasterserver"; //添加了(char*)
 char *password = (char *)"12345678";     //
 
+bool HitFlag = 0;
+
 CRGB Main_LEDS[Main_LED_NUM];
 CRGB Side_LEDS[Side_LED_NUM];
 CRGB Panel_LEDS[Panel_LED_NUM];
 
+void Main_LED(void *arg);
 void LED_Test();
 
-/* void Main_LED(*arg)
+void Main_LED(void *arg)
 {
+    /*
+                000111000
+                000011100
+                000001110
+                000000111
+                000000111
+                000001110
+                000011100
+                000111000
+                */
+    
+    bool Matrix[LED_STRIP_COLUMN][LED_STRIP_ROW];
+    for (int detal = 0;detal < count; i++)
+    {
+        /* code */
+    }
+    
+    for (int i = 0; i < ; i++)
+    {
+        switch ((i + detal) % 9)
+        {
+        case 0:
+        {
+            Matrix[i][0] = 0;
+            Matrix[i][1] = 0;
+            Matrix[i][2] = 0;
+            Matrix[i][3] = 1;
+            Matrix[i][4] = 1;
+            Matrix[i][5] = 0;
+            Matrix[i][6] = 0;
+            Matrix[i][7] = 0;
+        }
 
-    vTaskDelay(1000);
+        case 1:
+        {
+            Matrix[i][0] = 0;
+            Matrix[i][1] = 0;
+            Matrix[i][2] = 1;
+            Matrix[i][3] = 1;
+            Matrix[i][4] = 1;
+            Matrix[i][5] = 1;
+            Matrix[i][6] = 0;
+            Matrix[i][7] = 0;
+        }
+        case 2:
+        {
+            Matrix[i][0] = 0;
+            Matrix[i][1] = 1;
+            Matrix[i][2] = 1;
+            Matrix[i][3] = 1;
+            Matrix[i][4] = 1;
+            Matrix[i][5] = 1;
+            Matrix[i][6] = 1;
+            Matrix[i][7] = 0;
+        }
+        case 3:
+        {
+            Matrix[i][0] = 1;
+            Matrix[i][1] = 1;
+            Matrix[i][2] = 1;
+            Matrix[i][3] = 0;
+            Matrix[i][4] = 0;
+            Matrix[i][5] = 1;
+            Matrix[i][6] = 1;
+            Matrix[i][7] = 1;
+        }
+        case 4:
+        {
+            Matrix[i][0] = 1;
+            Matrix[i][1] = 1;
+            Matrix[i][2] = 0;
+            Matrix[i][3] = 0;
+            Matrix[i][4] = 0;
+            Matrix[i][5] = 0;
+            Matrix[i][6] = 1;
+            Matrix[i][7] = 1;
+        }
+        case 5:
+        {
+            Matrix[i][0] = 1;
+            Matrix[i][1] = 0;
+            Matrix[i][2] = 0;
+            Matrix[i][3] = 0;
+            Matrix[i][4] = 0;
+            Matrix[i][5] = 0;
+            Matrix[i][6] = 0;
+            Matrix[i][7] = 1;
+        }
+        case 6:
+        case 7:
+        case 8:
+        }
+
+        vTaskDelay(1000);
+    }
+
+    // vTaskDelay(1000);
 }
- */
+
 void LED_Test()
 {
     for (int i = 0; i < Main_LED_NUM; i++)
